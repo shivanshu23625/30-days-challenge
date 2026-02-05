0d50a93be4e07e06aa27dc2cc1d52876887c889f
Shivanshu Pandey <pshivanshu173@gmail.com>
2026-02-05 23:21:42 +0530

    Day 6 - Fibonacci Series in C

---
+++ day6_fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int first = 0, second = 1, next;
+
+    printf("Enter how many terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series:\n");
+
+    for(i = 1; i <= n; i++)
+    {
+        printf("%d ", first);
+
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return 0;
+}
